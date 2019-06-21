3b9a77bbd6fc6cdbeb3c8556d5968808753e6157
Luke Davies <lkdvs3@gmail.com>
2019-06-21 18:34:26 +0100

    Fixed eslint failing if no test directory exists

--- scripts/eslint.sh
+++ scripts/eslint.sh
@@ -2,10 +2,15 @@
 set -e
 
 FIX_ARGS=
+DIRS="src"
 
 if [ "$1" = "fix" ]; then
-  FIX_ARGS=--fix
+  FIX_ARGS="--fix"
+fi
+
+if [ -d "test" ]; then
+  DIRS="$DIRS test"
 fi
 
 echo "<<< Linting >>>"
-./node_modules/.bin/eslint --ext .js,.ts,.jsx,.tsx,.vue $FIX_ARGS src test
+./node_modules/.bin/eslint --ext .js,.ts,.jsx,.tsx,.vue $FIX_ARGS $DIRS
--- scripts/prettier.sh
+++ scripts/prettier.sh
@@ -4,7 +4,7 @@ set -e
 FIX_ARGS=
 
 if [ "$1" = "fix" ]; then
-  FIX_ARGS=--write
+  FIX_ARGS="--write"
 fi
 
 echo "<<< Formatting >>>"
